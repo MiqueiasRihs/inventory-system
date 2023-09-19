640c855edc4ed08a15b8c871361c134b248e5369
Miqueias <miqueias.rihs@gmail.com>
2023-09-18 21:51:25 -0300

    create product inventory endpoint updated to accept list

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import Session
 from sql import crud, models, schemas
 from sql.database import SessionLocal, engine
 
+from typing import List
+
+import json
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -17,12 +21,19 @@ def get_db():
         db.close()
 
 
-@app.post("/cadastrar-produto", response_model=schemas.Inventory)
-def register_product(product: schemas.InventoryCreate, db: Session = Depends(get_db)):
-    inventory = crud.get_inventory_by_id(db, id=product.id)
-    if inventory:
-        raise HTTPException(status_code=400, detail="Este produto já existe, tente atualiza-lo")
-    return crud.create_inventory(db=db, inventory=product)
+@app.post("/cadastrar-produto", response_model=List[schemas.Inventory])
+def register_product(products: List[schemas.InventoryCreate], db: Session = Depends(get_db)):
+    created_products = []
+    
+    for product in products:
+        inventory = crud.get_inventory_by_id(db, id=product.id)
+        if inventory:
+            raise HTTPException(status_code=400, detail=f"O produto com ID {product.id} já existe, tente atualizá-lo")
+        
+        created_product = crud.create_inventory(db=db, inventory=product)
+        created_products.append(created_product)
+    
+    return created_products
 
 
 @app.put("/atualizar-produto", response_model=schemas.Inventory)
